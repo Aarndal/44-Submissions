1658db9b8910d7067a1b677de3a920cf4ec35fb9
Aaron Dahlhauser <aarndal@duck.com>
2024-07-14 11:57:25 +0200

    Condition
    First experiment with a Condition class.

---
+++ AI Behaviour/Assets/_Project/Scripts/AIBehaviour/FSM_Base/Condition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Condition : IPredicate
+{
+    readonly Func<bool> func;
+
+    public Condition(Func<bool> func)
+    {
+        this.func = func;
+    }
+
+    public bool IsMet() => func.Invoke();
+}

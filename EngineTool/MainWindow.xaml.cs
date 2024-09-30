fab95faad22e8b7340459af0dcb184a694a6911a
Aarndal <aarndal@duck.com>
2024-09-30 20:10:40 +0200

    EngineTool - MVVM Init

--- EngineTool/MainWindow.xaml.cs
+++ EngineTool/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace EngineTool
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
---
+++ EngineTool/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace EngineTool
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private float _mapSize;
+
+        public MainWindowViewModel()
+        {
+            MapSize = 100;
+        }
+
+        public float MapSize
+        {
+            get => _mapSize;
+            set
+            {
+
+                if (_mapSize != value)
+                {
+                    _mapSize = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = "") // CallerMemberName: Automatically gets the name of the calling property if no string is provided
+        {
+            if (!string.IsNullOrEmpty(propertyName))
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
